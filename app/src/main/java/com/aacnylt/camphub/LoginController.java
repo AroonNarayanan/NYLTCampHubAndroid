579aada388c9bc438d41e0b49aeaf397edeafe12
Aroon Narayanan <aroonnarayanan@gmail.com>
2017-06-13 00:04:11 -0400

    Working on login and REST controller; need to improve async handling

--- app/src/main/java/com/aacnylt/camphub/LoginController.java
+++ app/src/main/java/com/aacnylt/camphub/LoginController.java
@@ -2,19 +2,21 @@ package com.aacnylt.camphub;
 
 import com.loopj.android.http.*;
 
-import org.json.*;
-
 import cz.msebera.android.httpclient.Header;
+
+import com.google.gson.*;
 
 public class LoginController {
     public static String host;
 
-    public static JSONObject authenticateUser(String username, String password, String server) {
+    public static Scout authenticateUser(String username, String password, String server) {
         host = server;
         String url = "/authenticate?username=" + username + "&password=" + password;
         RestController.Companion.get(url, null, new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject user) {
+            public void onSuccess(int statusCode, Header[] headers, JsonObject userJson) {
+                Gson gson = new Gson();
+                Scout user = gson.fromJson(userJson, Scout.class);
 
             }
         });
--- app/src/main/java/com/aacnylt/camphub/Scout.kt
+++ app/src/main/java/com/aacnylt/camphub/Scout.kt
@@ -1,4 +1,29 @@
 package com.aacnylt.camphub
 
+import java.util.*
+
 class Scout {
+    var FirstName: String = ""
+    var LastName: String = ""
+
+    var DateOfBirth: Date? = null
+    var Gender: String? =null
+    var ScoutID: Int = 0
+    var BSAID: Int? = 0
+    var Username: String? = null
+    var Password: String? = null
+
+    var Position: String? = null
+    var Team: String? = null
+
+    var IsAdmin: Boolean = false
+    var IsYouth: Boolean = false
+    var IsStaff: Boolean = false
+    var IsElevated: Boolean = false
+
+    var CourseName: String? = null
+    var CourseID: Int = 0
+
+    var LastModified: Date? = null
+    var Created: Date? = null
 }
